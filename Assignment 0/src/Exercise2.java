f0df106361465e555cd7b2a559f1469b44d34ded
Martino Mensio <martinomensio@outlook.it>
2016-10-10 14:46:24 +0200

    ass 0 ex 2

---
+++ Assignment 0/src/Exercise2.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.*;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// Get the "it.polito.dp2.file" system property
+		String dir = System.getProperty("it.polito.dp2.file");
+		GregorianCalendar max = null;
+		try {
+			max = Files.lines(Paths.get(dir))
+			.map((String s) -> {
+				String[] tokens = s.split("\\s");
+				Object[] numbers = Stream.of(tokens).map(s2 -> Integer.parseInt(s2)).toArray();
+				return new GregorianCalendar((Integer)numbers[5], (Integer)numbers[4], (Integer)numbers[3], (Integer)numbers[0], (Integer)numbers[1], (Integer)numbers[2]);
+			})
+			.max((a, b) -> a.compareTo(b))
+			.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		SimpleDateFormat fmt = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+		fmt.setCalendar(max);
+		String dateFormatted = fmt.format(max.getTime());
+		System.out.println(dateFormatted);
+	}
+
+}
