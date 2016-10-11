e90087ecfd301584790704eabbb0a9faf285b871
Martino Mensio <martinomensio@outlook.it>
2016-10-11 23:16:16 +0200

    assignment 0 commented

--- Assignment 0/src/Exercise1.java
+++ Assignment 0/src/Exercise1.java
@@ -1,3 +1,9 @@
+/* 
+ * The program is launched with a parameter that identifies a text file to be elaborated using a dictionary:
+ * all the words in the input file that don't appear in the dictionary file are echoed on standard output.
+ * 
+ * This solution uses streams, lambda expressions and "nio" library.
+ */
 
 import java.io.*;
 import java.nio.file.*;
@@ -11,11 +17,16 @@ public class Exercise1 {
 			return;
 		}
 		try {
+			// Read all the lines of the dictionary, to be ready to process the real input file
 			List<String> dictionaryRows = Files.readAllLines(Paths.get("dictionary.txt"));
+			// Now read the lines of the input file and process them (one by one) using a stream
 			Files.lines(Paths.get(args[0]))
+			// remove lines that appear in the dictionary
 			.filter(s -> !dictionaryRows.contains(s))
+			// and echo to standard output the remaining ones
 			.forEach(s -> System.out.println(s));
 		} catch (IOException e) {
+			// This exception can be thrown from one of the file reads, e.g. if input file does not exist with the specified name
 			System.err.println("IO exception with file " + e.getMessage());
 			e.printStackTrace();
 		}
--- Assignment 0/src/Exercise3.java
+++ Assignment 0/src/Exercise3.java
@@ -1,3 +1,8 @@
+/*
+ * This program merges two files into a third one. Names of the three files are specified as command line parameters.
+ * 
+ * This solution uses streams, lambda expressions and "nio" library.
+ */
 
 import java.io.*;
 import java.nio.file.*;
@@ -13,16 +18,23 @@ public class Exercise3 {
 		}
 		
 		try {
+			// Read all the lines from the first file
 			List<String> rows = Files.readAllLines(Paths.get(args[0]));
+			// Read all the lines from the second file
 			rows.addAll(Files.readAllLines(Paths.get(args[1])));
 			
+			// Use streams to process all rows
 			List<String> sortedRows = rows.stream()
+			// sort them (ascending)
 			.sorted()
+			// and store into a list
 			.collect(Collectors.toList());
 			
+			// Finally write the sorted rows to the third file
 			Files.write(Paths.get(args[2]),sortedRows);
 			
 		} catch (IOException e) {
+			// This exception can be thrown from one of the file reads, e.g. if input file does not exist with the specified name
 			System.err.println("IO exception with file " + e.getMessage());
 			e.printStackTrace();
 		}
