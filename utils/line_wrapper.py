a60b7e5b563288f74458e3e15d707a451d09ac27
Martino Mensio <martinomensio@outlook.it>
2016-10-27 11:33:19 +0200

    tool for wrapping lines up to 80 characters
    instead of manually insert newlines to make text fit into 80 characters
    (pain on text modification), this python script creates a wrapped file
    with lines less than 80 characters.
    For interoperability reasons, lines have CRLF terminators

---
+++ utils/line_wrapper.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+if len(sys.argv) != 3:
+    print('Wrong usage. Call ' + sys.argv[0] + ' <input_file> <output_file>')
+    sys.exit()
+
+fin = open(sys.argv[1], 'r')
+fout = open(sys.argv[2], 'w')
+
+content = fin.readlines()
+
+for s in content:
+    line =  '\r\n'.join(line.strip() for line in re.findall(r'.{1,80}(?:\s+|$)', s))
+    fout.write(line + '\r\n')
+
+print ('file has been line-wrapped. Output in file ' + sys.argv[2])
