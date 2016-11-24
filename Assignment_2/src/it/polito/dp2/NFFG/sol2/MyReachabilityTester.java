a2fe89f941a277f36675f8fc8f47b4719bfab888
Martino Mensio <martinomensio@outlook.it>
2016-11-24 01:23:11 +0100

    first implementation of loadNFFG
    seems to work

--- Assignment_2/src/it/polito/dp2/NFFG/sol2/MyReachabilityTester.java
+++ Assignment_2/src/it/polito/dp2/NFFG/sol2/MyReachabilityTester.java
@@ -1,6 +1,11 @@
 package it.polito.dp2.NFFG.sol2;
 
 import java.net.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
 
 import it.polito.dp2.NFFG.*;
 import it.polito.dp2.NFFG.lab2.*;
@@ -27,9 +32,61 @@ public class MyReachabilityTester implements ReachabilityTester {
 		// clean the graphName (for failures)
 		graphName = null;
 
-		// TODO delete the previous graph
+		Client client = ClientBuilder.newClient();
 
-		// TODO upload the new NFFG
+		// TODO refactor deletion previous graph
+		Response resdel = client.target(uri.toString()).path("resource").path("nodes").request(MediaType.TEXT_XML)
+				.accept("*/*").delete();
+		if (resdel.getStatus() != 200) {
+			throw new ServiceException("impossible to delete the previous graph");
+		}
+
+		// TODO refactor node uploading
+		Map<NodeReader, String> nodeIds = new HashMap<>();
+
+		for (NodeReader nodeR : nffgR.getNodes()) {
+			Node node = new Node();
+			Property nameP = new Property();
+			nameP.setName("name");
+			nameP.setValue(nodeR.getName());
+			node.getProperty().add(nameP);
+			try {
+				Node res = client.target(uri.toString()).path("resource").path("node").request(MediaType.TEXT_XML)
+						.accept("*/*").post(Entity.entity(node, MediaType.APPLICATION_XML), Node.class);
+
+				nodeIds.put(nodeR, res.getId());
+			} catch (ResponseProcessingException e) {
+				throw new ServiceException("impossible to upload the node named " + nodeR.getName());
+			}
+
+		}
+
+		Map<LinkReader, String> linkIds = new HashMap<>();
+
+		// TODO refactor links uploading
+		for (LinkReader linkR : nffgR.getNodes().stream().flatMap(n -> n.getLinks().stream())
+				.collect(Collectors.toList())) {
+			Relationship relation = new Relationship();
+			relation.setDstNode(nodeIds.get(linkR.getDestinationNode()));
+			relation.setType("Connection");
+			
+			
+
+			try {
+				System.out.println("Trying to get id of node " + linkR.getSourceNode().getName());
+				System.out.println(nodeIds.get(linkR.getSourceNode()));
+				Relationship res = client.target(uri.toString()).path("resource").path("node")
+						.path(nodeIds.get(linkR.getSourceNode()))
+						.path("relationship")
+						.request(MediaType.TEXT_XML).accept("*/*")
+						.post(Entity.entity(relation, MediaType.APPLICATION_XML), Relationship.class);
+
+				linkIds.put(linkR, res.getId());
+				System.out.println(res.getId());
+			} catch (ResponseProcessingException e) {
+				throw new ServiceException("impossible to upload the node named " + linkR.getName());
+			}
+		}
 
 		// on success, overwrite the graphName
 		graphName = name;
