7fa5142d25cce4b30656abb6ba945b7d6ed636cb
Martino Mensio <martinomensio@outlook.it>
2016-12-19 17:42:44 +0100

    renaming

--- Assignment_3/src/it/polito/dp2/NFFG/sol3/client2/Client2NFFGVerifier.java
+++ Assignment_3/src/it/polito/dp2/NFFG/sol3/client2/Client2NFFGVerifier.java
@@ -1,15 +1,29 @@
 package it.polito.dp2.NFFG.sol3.client2;
 
+import java.net.URI;
 import java.util.*;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import it.polito.dp2.NFFG.*;
+import it.polito.dp2.NFFG.sol3.jaxb.ObjectFactory;
 
 /**
  * 
  * @author Martino Mensio
  *
  */
-public class Client2NFFGVerifier implements NffgVerifier {
+public class Client2NffgVerifier implements NffgVerifier {
+
+	private WebTarget target;
+	private ObjectFactory factory;
+	
+	public Client2NffgVerifier(URI uri) {
+		// TODO Auto-generated constructor stub
+		target = ClientBuilder.newClient().target(uri);
+		factory = new ObjectFactory();
+	}
 
 	@Override
 	public NffgReader getNffg(String nffgName) {
