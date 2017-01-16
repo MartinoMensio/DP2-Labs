1da3be672bbf5405c09415f89da470fe166096ce
Martino Mensio <martinomensio@outlook.it>
2017-01-16 21:33:30 +0100

    swagger annotations on resource

--- Assignment_3/src/it/polito/dp2/NFFG/sol3/service/Resource.java
+++ Assignment_3/src/it/polito/dp2/NFFG/sol3/service/Resource.java
@@ -6,6 +6,8 @@ import java.util.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
+import com.wordnik.swagger.annotations.*;
+
 import it.polito.dp2.NFFG.lab3.*;
 import it.polito.dp2.NFFG.sol3.service.jaxb.*;
 
@@ -17,6 +19,7 @@ import it.polito.dp2.NFFG.sol3.service.jaxb.*;
  */
 
 @Path("/")
+@Api(value = "/", description = "service for nffg and policies")
 @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 public class Resource {
@@ -32,12 +35,20 @@ public class Resource {
 	}
 
 	@GET
+	@ApiOperation(value = "test", notes ="ahaha")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK")
+	})
 	@Path("test")
 	public String test() {
 		return "hello";
 	}
 
 	@GET
+	@ApiOperation(value = "get the collection of NFFGs", notes ="")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK")
+	})
 	@Path("nffgs")
 	public List<Nffg> getNffgs() {
 		// TODO
@@ -45,6 +56,12 @@ public class Resource {
 	}
 
 	@POST
+	@ApiOperation(value = "store a new NFFG", notes ="the name of the NFFG is the id of the created resource")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "NFFG created"),
+			@ApiResponse(code = 409, message = "conflict because of already stored nffg with same name"),
+			@ApiResponse(code = 422, message = "validation failed")
+	})
 	@Path("nffgs")
 	public Response postNffg(Nffg nffg, @Context UriInfo uriInfo) {
 		// TODO validate nffg
@@ -62,6 +79,11 @@ public class Resource {
 	}
 	
 	@DELETE
+	@ApiOperation(value = "delete an NFFG", notes ="identifying the NFFG by its name")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "NFFG deleted"),
+			@ApiResponse(code = 404, message = "NFFG not found")
+	})
 	@Path("nffgs/{nffg_name}")
 	public Response deleteNffg(@PathParam("nffg_name") String nffgName) {
 		Nffg nffg = service.deleteNffg(nffgName);
@@ -72,6 +94,12 @@ public class Resource {
 	}
 	
 	@POST
+	@ApiOperation(value = "verify a policy without storing it", notes ="to be called on the child resource of the nffg of reference")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "verification done"),
+			@ApiResponse(code = 404, message = "NFFG not found"),
+			@ApiResponse(code = 422, message = "policy is not compliant with the schema")
+	})
 	@Path("nffgs/{nffg_name}/online_result")
 	public Policy verifyResultOnTheFly(Policy policy, @PathParam("nffg_name") String nffgName) {
 		Policy result = service.verifyResultOnTheFly(policy, nffgName);
@@ -83,6 +111,11 @@ public class Resource {
 	}
 	
 	@GET
+	@ApiOperation(value = "get a single NFFG", notes ="identified by its name")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK"),
+			@ApiResponse(code = 404, message = "NFFG not found")
+	})
 	@Path("nffgs/{nffg_name}")
 	public Nffg getNffg(@PathParam("nffg_name") String nffgName) throws NotFoundException {
 		Nffg result = service.getNffg(nffgName);
@@ -93,6 +126,13 @@ public class Resource {
 	}
 	
 	@PUT
+	@ApiOperation(value = "store a policy to a specified resource", notes ="both for creation and update, identified by policy name")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "policy updated"),
+			@ApiResponse(code = 201, message = "policy created"),
+			@ApiResponse(code = 412, message = "the referred nffg does not exist"),
+			@ApiResponse(code = 422, message = "validation of policy failed")
+	})
 	@Path("policies/{policy_name}")
 	public Response postPolicy(Policy policy, @PathParam("policy_name") String policyName, @Context UriInfo uriInfo) {
 		if(policy == null) {
@@ -111,12 +151,21 @@ public class Resource {
 	}
 	
 	@GET
+	@ApiOperation(value = "get the collection of policies", notes ="")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK")
+	})
 	@Path("policies")
 	public List<Policy> getPolicies() {
 		return service.getPolicies();
 	}
 	
 	@GET
+	@ApiOperation(value = "get a policy", notes ="identified by its name")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK"),
+			@ApiResponse(code = 404, message = "policy not found")
+	})
 	@Path("policies/{policy_name}")
 	public Policy getPolicy(@PathParam("policy_name") String policyName) {
 		Policy policy = service.getPolicy(policyName);
@@ -127,6 +176,11 @@ public class Resource {
 	}
 	
 	@DELETE
+	@ApiOperation(value = "Delete a policy", notes ="identified by its name")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "policy deleted"),
+			@ApiResponse(code = 404, message = "policy not found")
+	})
 	@Path("policies/{policy_name}")
 	public Response deletePolicy(@PathParam("policy_name") String policyName) {
 		Policy policy = service.deletePolicy(policyName);
@@ -137,6 +191,11 @@ public class Resource {
 	}
 	
 	@POST
+	@ApiOperation(value = "Evaluate the result of a policy", notes ="The policy is verified using neo4j")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "result updated"),
+			@ApiResponse(code = 404, message = "policy not found")
+	})
 	@Path("policies/{policy_name}/result")
 	public Policy updatePolicyResult(@PathParam("policy_name") String policyName) {
 		Policy policy = service.updatePolicyResult(policyName);
