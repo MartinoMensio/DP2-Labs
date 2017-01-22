a8f2568aa8b7b5c299934e2bd9bd4bb5f6d6f7f1
Martino Mensio <martinomensio@outlook.it>
2017-01-22 18:14:39 +0100

    validation generic (both Nffg and Policy)

--- Assignment_3/src/it/polito/dp2/NFFG/sol3/service/validation/ValidatingReader.java
+++ Assignment_3/src/it/polito/dp2/NFFG/sol3/service/validation/ValidatingReader.java
@@ -17,15 +17,15 @@ import it.polito.dp2.NFFG.sol3.service.exceptions.*;
 import it.polito.dp2.NFFG.sol3.service.jaxb.*;
 
 @Provider
-@Consumes({ MediaType.APPLICATION_XML, MediaType.TEXT_XML })
-public class NffgValidatingReader implements MessageBodyReader<Nffg> {
+@Consumes({ MediaType.APPLICATION_XML })
+public class ValidatingReader<T> implements MessageBodyReader<T> {
 
 	final String jaxbPackage = "it.polito.dp2.NFFG.sol3.service.jaxb";
 	Unmarshaller unmarshaller;
 
-	public NffgValidatingReader() {
+	public ValidatingReader() {
 		try {
-			InputStream schemaStream = NffgValidatingReader.class.getResourceAsStream("/xsd/nffgVerifier.xsd");
+			InputStream schemaStream = ValidatingReader.class.getResourceAsStream("/xsd/nffgVerifier.xsd");
 			if (schemaStream == null) {
 				throw new IOException("schema file not found");
 			}
@@ -42,25 +42,36 @@ public class NffgValidatingReader implements MessageBodyReader<Nffg> {
 
 	@Override
 	public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-		return Nffg.class.equals(type) && jaxbPackage.equals(type.getPackage().getName())
-				&& (mediaType.equals(MediaType.APPLICATION_XML) || mediaType.equals(MediaType.APPLICATION_JSON));
+		return (Nffg.class.equals(type) || Policy.class.equals(type))
+				&& jaxbPackage.equals(type.getPackage().getName());
 	}
 
 	@Override
-	public Nffg readFrom(Class<Nffg> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+	public T readFrom(Class<T> type, Type genericType, Annotation[] annotations, MediaType mediaType,
 			MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
 			throws IOException, WebApplicationException {
 		if (unmarshaller == null) {
 			throw new InternalServerErrorException("unmarshaler null");
 		}
+
 		try {
-			return (Nffg) unmarshaller.unmarshal(entityStream);
+			return castToGeneric(unmarshaller.unmarshal(entityStream));
 		} catch (JAXBException ex) {
 			Throwable linked = ex.getLinkedException();
 			String validationErrorMessage = "Validation error";
 			if (linked != null && linked instanceof SAXParseException)
 				validationErrorMessage += ": " + linked.getMessage();
+			// TODO distinguish malformed from invalid (reference to schema)
 			throw new ValidationFailedException(validationErrorMessage);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private T castToGeneric(Object o) {
+		try {
+			return (T) o;
+		} catch (ClassCastException e) {
+			throw new InternalServerErrorException();
 		}
 	}
 
