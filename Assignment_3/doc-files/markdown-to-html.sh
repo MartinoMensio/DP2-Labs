fec16bb02806c1cdaf04713f8f0c5aa0bbf7dbf9
Martino Mensio <martinomensio@outlook.it>
2017-01-22 10:32:08 +0100

    title fix for web documentation

--- Assignment_3/doc-files/markdown-to-html.sh
+++ Assignment_3/doc-files/markdown-to-html.sh
@@ -1,2 +1,2 @@
 # requires markdown-to-html (from npm)
-github-markdown --stylesheet markdown.css ../doc/nffgVerifier.md > ../WebContent/index.html
+github-markdown --stylesheet markdown.css --title "NffgService API" ../doc/nffgVerifier.md > ../WebContent/index.html
