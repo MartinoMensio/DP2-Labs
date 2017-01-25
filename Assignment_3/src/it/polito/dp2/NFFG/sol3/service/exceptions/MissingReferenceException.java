a8674b5072733f698bd1e9278c4448a318298efe
Martino Mensio <martinomensio@outlook.it>
2017-01-25 18:06:40 +0100

    not using HTTP status 422
    refers #42

--- Assignment_3/src/it/polito/dp2/NFFG/sol3/service/exceptions/MissingReferenceException.java
+++ Assignment_3/src/it/polito/dp2/NFFG/sol3/service/exceptions/MissingReferenceException.java
@@ -4,16 +4,13 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 /**
- * This class handles the HTTP 422 status code, that is meant for requests that
- * have right syntax but are semantically incorrect. See
- * <a href="https://tools.ietf.org/html/rfc4918#section-11.2">corresponding
- * RFC</a> This exception is used when there is a reference to some data that is
+ * This exception is used when there is a reference to some data that is
  * not stored in the service.
  * 
  * @author Martino Mensio
  *
  */
-public class MissingReferenceException extends ClientErrorException {
+public class MissingReferenceException extends BadRequestException {
 
 	/**
 	 * 
@@ -21,8 +18,7 @@ public class MissingReferenceException extends ClientErrorException {
 	private static final long serialVersionUID = 1L;
 
 	public MissingReferenceException(String string) {
-		// need all this because 422 is not included in enum Response.Status
-		super(Response.status(422).entity("Missing reference: " + string).type(MediaType.TEXT_PLAIN).build());
+		super("Missing reference: " + string);
 	}
 
 }
